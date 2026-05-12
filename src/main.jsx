bcfa70cf1fb7fdb7ee5f497b14f52232c0395e7f
vladisalex-it <vl4disalex@gmail.com>
2026-05-13 00:44:50 +0300

    fix: remove basename from HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter basename="/drift-map">
+    <HashRouter>
       <FavoritesProvider>
         <App />
       </FavoritesProvider>
